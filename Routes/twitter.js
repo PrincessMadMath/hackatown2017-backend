809912fd3d2b2436a12a5cdeec44d141258b7dfa
Alex Gagné <alex_gagne94@hotmail.com>
2017-02-04 16:58:55 -0500

    minor fix

--- Routes/twitter.js
+++ Routes/twitter.js
@@ -1,5 +1,6 @@
 var express = require('express')
-var twitter = require('twitter')
+var twitter = require('twitter')  
+var env = require('node-env-file');
 
 const router = express.Router()
 
@@ -11,29 +12,34 @@ var client = new twitter({
     });
 
 
-router.get('/sigin', function (req, res) {
-  res.json({"message": "Don't call this"})
+router.get('/signin', function (req, res) {
+    res.json({"message": "Don't call this"})
 })
 
 router.get('/feedbyhashtag/:hashtag', function (req, res) {
-    res.json(getTweetsByHashtag(req.params.hashtag))
-})
-
-router.get('/monparc'), function (req, res){
-    res.json(getTweetsByHashtag('monparc'))
-}
-
-const getTweetsByHashtag = (hashtag) => {
-    var query = '#' + hashtag;
+    var query = '#' + req.params.hashtag;
     client.get('search/tweets', { q: query, count: 10 }, function(err, data, response) {
         if(!err){
-            return data;
+            res.json(data);
         }
         else{
-            console.log(err);
+            res.json(err);
             throw err;
         }
     })
-}
+})
+
+router.get('/monparc', function (req, res){
+    var query = '#' + 'monparc';
+    client.get('search/tweets', { q: query, count: 10 }, function(err, data, response) {
+        if(!err){
+            res.json(data);
+        }
+        else{
+            res.json(err);
+            throw err;
+        }
+    })
+})
 
 module.exports = router
