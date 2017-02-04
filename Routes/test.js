a3973e72ab73c0f8a800dbccbb3758b3dc5a9334
Mathieu Gamache <mathg99@hotmail.com>
2017-02-04 15:03:11 -0500

    Variable testing

--- Routes/test.js
+++ Routes/test.js
@@ -3,8 +3,18 @@ const router = express.Router()
 
 
 router.get('/', function (req, res) {
-  res.json({"message": "Hello World!!!!!!"})
+  res.json({"message": "Hello World!"})
 })
+
+router.get('/public', function (req, res) {
+  res.json({"public": process.env.TEST_PUBLIC})
+})
+
+router.get('/secret', function (req, res) {
+  res.json({"secret": process.env.TEST_SECRET})
+})
+
+
 
 
 module.exports = router
