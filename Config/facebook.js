8bca8cabbb52035b35ed58eebc520cd06a310301
Mathieu Gamache <mathg99@hotmail.com>
2017-02-05 00:51:02 -0500

    Update key config facebook

--- Config/facebook.js
+++ Config/facebook.js
@@ -6,7 +6,8 @@ if(process.env.IS_AWS)
 { 
     config = { 
         client_id: process.env.FB_PUBLIC_ID, 
-        client_secret: process.env.FB_PRIVATE_ID 
+        client_secret: process.env.FB_PRIVATE_ID,
+        client_token: process.env.FACEBOOK_CLIENT_TOKEN
     } 
 } 
 else{ 
